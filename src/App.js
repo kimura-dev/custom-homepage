6c31c54b530f4aa1143ee45d7938299e0bfd19a5
Scott Reyell <smreyell@gmail.com>
2020-08-09 12:15:34 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import Joke from './Joke';
+import Stories from './Stories';
+
+function App() {
+	const [ userQuery, setUserQuery ] = useState('');
+
+	const updateUserQuery = (event) => {
+		console.log('userQuery', userQuery);
+		setUserQuery(event.target.value);
+	};
+
+	const searchQuery = () => {
+		// Allows you to go to another url
+		window.open(`https://google.com/search?q=${userQuery}`, '_blank');
+	};
+
+	const handleKeyPress = (event) => {
+		if (event.key === 'Enter') {
+			searchQuery();
+		}
+	};
+
+	return (
+		<div className='App'>
+			<h1>Hello Scott</h1>
+			<div className='form'>
+				<input value={userQuery} onChange={updateUserQuery} onKeyPress={handleKeyPress} />
+				<button onClick={searchQuery}>Search</button>
+			</div>
+			<hr />
+			<Joke />
+			<hr />
+			<Stories />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/hooks.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+export const useFetch = (url, initialValue) => {
+	const [ result, setResult ] = useState(initialValue);
+
+	useEffect(() => {
+		fetch(url).then((res) => res.json()).then((json) => setResult(json));
+	}, []);
+
+	return result;
+};
